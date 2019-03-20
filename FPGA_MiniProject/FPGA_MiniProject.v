5b2af705a93b51fed936dc3bf4d0431297c24754
Haider Shafiq <haider.shafiq.96@gmail.com>
2019-03-20 12:19:41 +0000

    Further setup
    Just done further setup in the top level module

--- FPGA_MiniProject/FPGA_MiniProject.v
+++ FPGA_MiniProject/FPGA_MiniProject.v
@@ -1,1 +1,12 @@
-//Test for git.
+/* Top level modile for FPGA_MiniProject
+Alexander Bolton - 200938078
+Haider Shafiq - 201207577
+N-Channel Oscilloscope */
+
+module FPGA_MiniProject
+
+
+
+
+
+endmodule 
