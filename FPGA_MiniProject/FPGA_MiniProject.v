5e7aa16019c9e55d0badb97a486e3bb5d7595e98
Haider <haider.shafiq.96@gmail.com>
2019-03-24 15:21:53 +0000

    Test
    Just a test to get me reintroduced to verilog

--- FPGA_MiniProject/FPGA_MiniProject.v
+++ FPGA_MiniProject/FPGA_MiniProject.v
@@ -3,10 +3,18 @@ Alexander Bolton - 200938078
 Haider Shafiq - 201207577
 N-Channel Oscilloscope */
 
-module FPGA_MiniProject
+// Top Level Module
+module FPGA_MiniProject #(
+			// Number of Channels -> Start with 1 
+			parameter Channels = 1
+)(
+			// Following code is quick test to remind me of how verilog works 
+			input  a,
+			input  b,
+			output c
+);
 
-
-
-
+			// Should see an & gate in the RTL viewer
+			assign c = a & b;
 
 endmodule 
