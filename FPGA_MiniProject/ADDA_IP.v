d9f9902d56762cfa36b45f088ff24b8da520fad4
Alexander Bolton <alex@hectospark.co.uk>
2019-03-27 11:50:30 +0000

    ADDA IP

---
+++ FPGA_MiniProject/ADDA_IP.v
@@ -0,0 +1,21 @@
+//8192 Levels per side (+ve,-ve)
+//2Vpk-pk max in
+
+module ADDA(
+input 			clock,				//clock
+input [13:0] 	ADC,					//ADDA GPIO
+input 			OutOfRange,			//OUT OF RANGE INDICATOR
+input 			EN,					//CHANNEL ENABLE PIN
+input				clockEN,				//CLOCK ENABLE
+input				invertEN,			//INVERT OUTPUT
+output [13:0] 	ADCOut,				
+output 			OutOfRangeOut,
+output			clockOut
+);
+localparam signd = false;
+wire signalOut[13:0];
+Assign clockOut = (clockEN == 1) ? clock : 0;
+Assign signalOut = (invertEN == 1) ? ~ADC : ADC;
+Assign ADCOut = (EN == 1) ? signalOut : 0;
+Assign OutOfRangeOut = OutOfRangep;
+endmodule 
