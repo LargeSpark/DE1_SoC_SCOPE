a20583a0e944d438a7aa7bff75253640b4d0f650
Alexander Bolton <alex@hectospark.co.uk>
2019-04-10 20:27:05 +0100

    Bug #1 - Need to fix reset sticking

--- FPGA_MiniProject/VGA_IP.v
+++ FPGA_MiniProject/VGA_IP.v
@@ -64,7 +64,6 @@ reg [Vregsize_d:0] v_d_counter  = 0;
 //Positions counter initilisations
 reg [Hozregsize:0] HozPixel = 0;
 reg [Verregsize:0] VerPixel = 0;
-
 //Indicator for section of signal
 reg [2:0] HozsigIndicator = 0;
 /*
@@ -90,7 +89,6 @@ assign G = (HozsigIndicator == 2 && VerSigOn == 0)? colour_G : 0;
 assign B = (HozsigIndicator == 2 && VerSigOn == 0)? colour_B : 0;
 //assign vsync
 assign vga_vsync = (VerSigIndicator == 0 && VerSigOn == 1 && rstV == 0) ? 0 : 1;
-
 //counters
 always @(posedge clock) begin
 	//If sync
@@ -115,9 +113,13 @@ always @(posedge clock) begin
 	
 	//if data
 	if(HozsigIndicator == 2) begin
-		if(h_c_counter == h_c_endcount) begin
+		if(h_c_counter == Horizontal_Size) begin //changed to work with counting pixels
 			h_c_counter <= 0;
 			HozsigIndicator <= 3;
+			HozPixel <= 0;
+			if(VerSigOn == 0) begin
+				VerPixel <= VerPixel + 1;
+			end
 		end else begin
 		h_c_counter <= h_c_counter + 1;
 		end
@@ -138,25 +140,29 @@ end
 //Pixel Counter & V Sync
 always @(posedge clock) begin
 	//##Counter
-	
 	if(VerSigOn == 0) begin
-		if(HozPixel < Horizontal_Size) begin
-				HozPixel <= HozPixel + 1; //add one to position
-		end else begin
+	//always one behind
+		if(h_c_counter == 640 || Hsync == 0) begin
 			HozPixel <= 0;
-			VerPixel <= VerPixel+1;
-			
+		end
+		else begin
+			HozPixel <= h_c_counter + 1;
+		end
+	end
+	if(VerSigOn == 0 && vga_hsync == 1 && HozsigIndicator == 1) begin
 			if(VerPixel >= Vertical_Size) begin
 				VerSigOn <= 1;
 			end
-		end
+	end
+	//TEST CODE
+	if(vga_hsync == 0) begin
+		HozPixel = 0;
 	end
 	
 	if(rstV == 1) begin
 		HozPixel <= 0;
 		VerPixel <= 0;
 		VerSigOn <= 0;
-		rstV <= 0;
 	end
 	//end
 	
@@ -166,7 +172,7 @@ always @(posedge clock) begin
 end
 
 //VerPixel == 0;
-reg [0:0] rstcounter = 1;
+reg rstcounter = 1;
 //Vsync
 always @(posedge vga_hsync) begin
 
@@ -216,6 +222,7 @@ always @(posedge vga_hsync) begin
 			rstcounter <= rstcounter + 1;
 			if(rstcounter == 1) begin
 			rstcounter <= 0;
+			rstV <= 0;
 			end
 		end
 	end
