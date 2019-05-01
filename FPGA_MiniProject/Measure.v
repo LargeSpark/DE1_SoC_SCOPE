f48c634e2099087ce76272db559e51a8b66f6134
Haider <haider.shafiq.96@gmail.com>
2019-05-01 16:48:03 +0100

    #4 Subtraction working, need to fix measure

--- FPGA_MiniProject/Measure.v
+++ FPGA_MiniProject/Measure.v
@@ -22,35 +22,19 @@ reg [13:0] deltax2 = 0;
 reg [13:0] result = 6;
 assign num = result;
 
-reg [13:0] Diffx; //Delta x
+wire [13:0] Diffx; //Delta x
 //Find Delta x - makes sure we get no -ve values 
-//assign Diffx = (deltay1 < 0) ? deltay2 : deltay1;
-reg [13:0] Diffy; //Delta y
+assign Diffx = (deltay1 < 0) ? deltay2 : deltay1;
+wire [13:0] Diffy; //Delta y
 //Find Delta y 
-//assign Diffy = (deltax1 < 0) ? deltax2 : deltax1;
+assign Diffy = (deltax1 < 0) ? deltax2 : deltax1;
 
 always @(posedge buttonClock) begin 
 	deltay1 <= cursory1 - cursory2;
 	deltay2 <= cursory2 - cursory1;
 	deltax1 <= cursorx1 - cursorx2;
 	deltax2 <= cursorx2 - cursorx1;
-	//result <= Diffx;
-	if (deltax1 < 0)
-	begin 
-		Diffy <= deltax2;
-	end 
-	else if (deltax1 > 0)
-	begin
-		Diffy <= deltax1;
-	end
-	else if (deltay1 < 0)
-	begin 
-		Diffx <= deltay2;
-	end 
-	else if (deltay1 > 0)
-	begin
-		Diffx <= deltay1;
-	end
+	result <= Diffx;
 end
 
 endmodule 
